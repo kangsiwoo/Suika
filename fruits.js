f4a500b2ec15b2db97632d4a6d79fb944228021b
kangsiwoo <2023003@bssm.hs.kr>
2023-12-02 18:02:53 +0000

    ADD : generate and ready to drop

---
+++ fruits.js
@@ -0,0 +1,59 @@
+const FRUITS_BASE = [
+    {
+        name: "base/00_cherry",
+        radius: 33 / 2,
+        color: '#D2042D'
+    },
+    {
+        name: "base/01_strawberry",
+        radius: 48 / 2,
+        color: '#FB2943'
+    },
+    {
+        name: "base/02_grape",
+        radius: 61 / 2,
+        color: '#6C3461'
+    },
+    {
+        name: "base/03_gyool",
+        radius: 69 / 2,
+        color: '#ffbc21'
+    },
+    {
+        name: "base/04_orange",
+        radius: 89 / 2,
+        color: '#FFA500'
+    },
+    {
+        name: "base/05_apple",
+        radius: 114 / 2,
+        color: '#66B447'
+    },
+    {
+        name: "base/06_pear",
+        radius: 129 / 2,
+        color: '#C9CC3F'
+    },
+    {
+        name: "base/07_peach",
+        radius: 156 / 2,
+        color: '#ff87f1'
+    },
+    {
+        name: "base/08_pineapple",
+        radius: 177 / 2,
+        color: '#FEEA63'
+    },
+    {
+        name: "base/09_melon",
+        radius: 220 / 2,
+        color: '#FDBCB4'
+    },
+    {
+        name: "base/10_watermelon",
+        radius: 259 / 2,
+        color: '#D23B68'
+    },
+];
+
+export { FRUITS_BASE };
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {Bodies, Engine, Render, Runner, World} from 'matter-js'
-import {FRUITS} from './fruits.js'
+import {FRUITS_BASE} from './fruits.js'
 
 const engine = Engine.create()
 const render = Render.create({
@@ -30,6 +30,7 @@ const ground = Bodies.rectangle(310, 820, 620, 60, {
 })
 
 const topLine = Bodies.rectangle(310, 150, 620, 2, {
+    isSensor: true,
     isStatic: true,
     render: { fillStyle: '#e6b143'}
 })
@@ -38,3 +39,22 @@ World.add(world, [leftWall, rightWall, ground, topLine])
 
 Render.run(render)
 Runner.run(engine)
+
+function AddFruit() {
+    const index = Math.floor(Math.random() * 5)
+    const fruit = FRUITS_BASE[index]
+
+    console.log(fruit.color)
+
+    const body = Bodies.circle(300, 50, fruit.radius, {
+        index: index,
+        isSleeping: true,
+        render: {
+            fillStyle: `${fruit.color}`
+        },
+        restitution: 0.2
+    })
+    World.add(world, body);
+}
+
+AddFruit()
